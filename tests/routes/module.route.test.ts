60d92a7475de825ac7bb66f1b2aada8952eca42d
JoseLozadaAD <jose.lozada@training.assuresoft.com>
2023-11-30 18:00:29 -0400

    Fixed validation problems

--- tests/routes/module.route.test.ts
+++ tests/routes/module.route.test.ts
@@ -11,20 +11,29 @@ jest.mock('../../src/controllers/module.controller');
 
 describe('Module Routes', () => {
   it('should respond with status 200 for POST /', async () => {
+    const mockModule = {
+      name: 'Test Module',
+      description: 'Testing module',
+      trainerId: 1,
+      startDate: String(new Date()),
+      endDate: String(new Date()),
+      schedule: '10:30 - 12:00',
+    };
+
     (createModuleController as jest.Mock).mockImplementation(
       (request, response) => response.status(201).json()
     );
 
-    const response = await request(app).post('/').send();
+    const response = await request(app).post('/').send(mockModule);
     expect(response.status).toBe(201);
   });
 
   it('should respond with status 500 for POST /', async () => {
     (createModuleController as jest.Mock).mockImplementation(
-      (request, response) => response.status(500).json()
+      (request, response) => response.status(400).json()
     );
 
     const response = await request(app).post('/').send();
-    expect(response.status).toBe(500);
+    expect(response.status).toBe(400);
   });
 });
