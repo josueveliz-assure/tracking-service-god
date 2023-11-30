be01157490861d93fcd39d66ebc482d69e03196d
jacovzap-assuresoft <jacobo.covarrubias@training.assuresoft.com>
2023-11-30 09:38:29 -0400

    validation middleware with yup

--- src/routes/module.route.ts
+++ src/routes/module.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createModuleController } from '../controllers/module.controller';
+import { validateModuleData } from '../middlewares/validateData';
 
 const router = Router();
 
-router.post('/', createModuleController);
+router.post('/', validateModuleData, createModuleController);
 
 export default router;
