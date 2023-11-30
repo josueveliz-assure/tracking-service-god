285cc75913e9fa5c2e418633bd63a428c3293126
JoseLozadaAD <jose.lozada@training.assuresoft.com>
2023-11-30 17:43:44 -0400

    test

--- tests/controllers/module.controller.test.ts
+++ tests/controllers/module.controller.test.ts
@@ -1,14 +1,22 @@
 import request from 'supertest';
 import express from 'express';
-import { createModuleController } from '../../src/controllers/module.controller';
-import { createModuleRepository } from '../../src/repositories/module.repository';
+import {
+  addTraineesModuleController,
+  createModuleController,
+} from '../../src/controllers/module.controller';
+import {
+  createModuleRepository,
+  addTraineesModuleRepository,
+} from '../../src/repositories/module.repository';
 
 const app = express();
 app.use(express.json());
 app.post('/', createModuleController);
+app.put('/:id', addTraineesModuleController);
 
 jest.mock('../../src/repositories/module.repository', () => ({
   createModuleRepository: jest.fn(),
+  addTraineesModuleRepository: jest.fn(),
 }));
 
 describe('createModuleController', () => {
@@ -25,11 +33,11 @@ describe('createModuleController', () => {
       endDate: String(new Date()),
       schedule: '10:30 - 12:00',
     };
-    (createModuleRepository as jest.Mock).mockResolvedValueOnce(mockCreatedModule);
+    (createModuleRepository as jest.Mock).mockResolvedValueOnce(
+      mockCreatedModule,
+    );
 
-    const response = await request(app)
-      .post('/')
-      .send();
+    const response = await request(app).post('/').send();
 
     expect(response.status).toBe(201);
     expect(response.body).toEqual(mockCreatedModule);
@@ -39,11 +47,37 @@ describe('createModuleController', () => {
     const mockError = new Error('Repository error');
     (createModuleRepository as jest.Mock).mockRejectedValueOnce(mockError);
 
-    const response = await request(app)
-      .post('/')
-      .send();
+    const response = await request(app).post('/').send();
 
     expect(response.status).toBe(500);
-    expect(response.body).toEqual({ message: mockError.message, trace: mockError.stack });
+    expect(response.body).toEqual({
+      message: mockError.message,
+      trace: mockError.stack,
+    });
+  });
+
+  it('should respond with status 201 and return updated module', async () => {
+    const trainees = {
+      name: 'Juan',
+      lastName: 'Mamani',
+      email: 'juan.mamani@example.com',
+      grade: 90,
+    };
+    const mockCreatedModule = {
+      name: 'Test Module',
+      description: 'Testing module',
+      trainerId: 1,
+      startDate: String(new Date()),
+      endDate: String(new Date()),
+      schedule: '10:30 - 12:00',
+    };
+    (addTraineesModuleRepository as jest.Mock).mockResolvedValueOnce(
+      mockCreatedModule,
+    );
+
+    const response = await request(app).put('/1').send();
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(trainees);
   });
 });
