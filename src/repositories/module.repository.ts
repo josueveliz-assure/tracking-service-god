ccb3eefe52474781ccaefe6d364d4e707b99ba90
jacovzap-assuresoft <jacobo.covarrubias@training.assuresoft.com>
2023-11-30 19:43:06 -0400

    added feedback to a trainee inside a module

--- src/repositories/module.repository.ts
+++ src/repositories/module.repository.ts
@@ -1,5 +1,5 @@
 import ModuleModel from '../models/module';
-import { Module } from '../types/types';
+import { Module, Trainee, Feedback } from '../types/types';
 
 export const createModuleRepository = async (module: Module): Promise<Module> => {
   return ModuleModel.create(module);
@@ -7,4 +7,20 @@ export const createModuleRepository = async (module: Module): Promise<Module> =>
 
 export const addTraineesModuleRepository = async (id: string, trainees: Module): Promise<Module|null> => {
   return ModuleModel.findByIdAndUpdate(id, trainees, { new: true });
+};
+
+export const addFeedbackToTraineeRepository = async (moduleId: string, traineeId: string, feedback: Feedback): Promise<Module|undefined> => {
+  const module = await findModuleByIdRepository(moduleId);
+  const trainee = module?.trainees?.find((trainee: Trainee) => trainee.id === traineeId);
+
+  trainee?.feedback?.push(feedback);
+  return module?.save();
+};
+
+export const findModuleByIdRepository = async (id: string): Promise<Module|null> => {
+  return ModuleModel.findById(id);
+};
+
+export const findTraineeByIdRepository = async (moduleId: string, traineeId: string): Promise<Trainee|null> => {
+  return ModuleModel.findById(moduleId, { trainees: { $elemMatch: { id: traineeId } } });
 };
