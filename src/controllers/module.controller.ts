318271d68890c9978d26a4ddcf47f840b9e9dd13
JoseLozadaAD <jose.lozada@training.assuresoft.com>
2023-11-30 23:12:43 -0400

    Fixed bug

--- src/controllers/module.controller.ts
+++ src/controllers/module.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { createModuleRepository, addTraineesModuleRepository } from '../repositories/module.repository';
+import { createModuleRepository, addTraineesModuleRepository, findModuleByIdRepository } from '../repositories/module.repository';
 import { Module } from '../types/types';
 
 export const createModuleController = async (req: Request, res: Response) => {
@@ -19,9 +19,12 @@ export const createModuleController = async (req: Request, res: Response) => {
 export const addTraineesModuleController = async (req: Request, res: Response) => {
   try {
     const { body } = req;
-    const update: Partial<Module> = { trainees: [...body] };
 
+    const moduleFromDb = await findModuleByIdRepository(req.params.id);
+    
+    const update: Partial<Module> = { trainees: [...moduleFromDb?.trainees || [], ...body] };
     const moduleUpdated = await addTraineesModuleRepository(req.params.id, update as Module);
+    
     return res.status(201).json(moduleUpdated);
   } catch (error) {
     if (error instanceof Error) {
--- src/repositories/module.repository.ts
+++ src/repositories/module.repository.ts
@@ -5,6 +5,10 @@ export const createModuleRepository = async (module: Module): Promise<Module> =>
   return ModuleModel.create(module);
 };
 
+export const findModuleByIdRepository = async (id: string): Promise<Module|null> => {
+  return ModuleModel.findById(id);
+};
+
 export const addTraineesModuleRepository = async (id: string, trainees: Module): Promise<Module|null> => {
   return ModuleModel.findByIdAndUpdate(id, trainees, { new: true });
 };
